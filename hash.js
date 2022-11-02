c6fdc68eac5658a7fc91c1e539e9bef7357a194b
Silver <Silverg33k@gmail.com>
2022-11-02 17:27:28 +0100

    👍 Added vercel deployer to repository

---
+++ hash.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const api = express()
+
+api.get('/', (request, response) => {
+    response.status(200).json({
+        slackUsername: 'Silver',
+        backend: true,
+        age: 25,
+        bio: 'Getting shit done!'
+    })
+})
+
+//for endpoints that are not valid
+api.all('*', (request, response) => {
+    response.status(404).send('Not a valid route, try the endpoint without any path')
+});
+
+
+api.listen(8080, ()=> {
+    console.log(`Server running on port 8080`)
+})
